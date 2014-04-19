206c8b0b59a378bbde7f670ec86091e613e7b35a
Royceb <rbeezy707@gmail.com>
2014-04-18 17:11:58 -0700

    About page, etc

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.3'
 
+gem 'high_voltage'
+
 # Use postgresql as the database for Active Record
 gem 'pg'
 
@@ -34,6 +36,9 @@ end
 
 
 gem 'twitter-bootstrap-rails'
+gem 'therubyracer'
+gem 'less-rails'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 MyE::Application.routes.draw do
   resources :members
-  root :to => "members#index"
+  #root :to => "members#index"
+
+  get '/about'    => 'high_voltage/pages#show', id: 'about'
+  get '/contact'  => 'high_voltage/pages#show', id: 'contact'
+  get '/privacy'  => 'high_voltage/pages#show', id: 'privacy'
+  get '/terms'    => 'high_voltage/pages#show', id: 'terms'
+
+  get '/home', to: redirect('/')
+
+  root :to => 'high_voltage/pages#show', id: 'home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
